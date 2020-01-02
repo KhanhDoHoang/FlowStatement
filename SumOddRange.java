1c68db7ce0e2e725275b0f64cc9d593f3c058721
GuruFish <55791090+KhanhDoHoang@users.noreply.github.com>
2020-01-01 21:18:31 -0500

    Updated

--- SumOddRange.java
+++ SumOddRange.java
@@ -1,20 +1,59 @@
 import java.util.Scanner;
 public class SumOddRange {
-	public static void main (String[]args)
-	{
-		Scanner input = new Scanner (System.in);
-		int number = 0, end = 0, sum = 0;
+	    public static boolean isOdd(int number)
+	    {
+	        if(number > 0)
+	        {
+	        	if (!(number % 2 == 0))
+	        	{
+	        		return true;
+	        	}
+	        }
+	        	return false;
+	    }
+	    	
+	    //
+	    	
+	    
+	   
+	    public static int sumOdd (int start, int end)
+	    {
+	    	int sum = 0;
+	    	
+	    	
+	    	if (end >= start && end > 0 && start > 0)
+			{
+	    		for (int i = start; i <= end; i++)
+	    		{
+	    				
+	    				
+	    				if(isOdd(i))
+	    				{
+	    						sum += i;
+	    				}
+	    		}
+	    		return sum;
+			}
+	    return -1;	
+	   }
+	     
+	   
+	   public static void main (String[]args)
+	   {
+		   int start = 0, end = 0;
+		   Scanner input = new Scanner(System.in);
+		   System.out.println("Enter start num: ");
+		   start = input.nextInt();
+		   System.out.println("Enter end num: ");
+		   end = input.nextInt();
+		   
+		   
+		   
+		   
+		  System.out.println(sumOdd(start, end));
+		   
 		
-		System.out.println("Enter first number: ");
-		number = input.nextInt();
-		
-		System.out.println("Enter last number: ");
-		end = input.nextInt();
-		
-		for (int i = 0; i < end; i++)
-		{
-			
-			number += i;
-		}
-	}
+		   
+		   
+	   }
 }
